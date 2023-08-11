ca4a4f93b55113fed8dc6972d286cf2ff400e3f3
TuncayGulec <132682717+TuncayGulec@users.noreply.github.com>
2023-08-11 10:41:16 +0300

    patikaCalisma

---
+++ Sigorta/src/Address/Address.java
@@ -0,0 +1,19 @@
+package Address;
+
+public class Address {
+    public String getStreet();
+
+    public void setStreet(String street);
+
+    public String getCity();
+
+    public void setCity(String city);
+
+    public String getCountry();
+
+    public void setCountry(String country);
+
+    public String getPostalCode();
+
+    public void setPostalCode(String postalCode);
+}
